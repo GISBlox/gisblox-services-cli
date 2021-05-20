974f42a42e74aa69ece37e12ccf4dc1e542a28ef
Karlo Bartels <karlo@bartelsonline.nl>
2021-05-20 14:20:39 +0200

    initial setup

--- GISBlox.Services.CLI/GISBlox.Services.CLI/Program.cs
+++ GISBlox.Services.CLI/GISBlox.Services.CLI/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using GISBlox.Services.SDK;
 
 namespace GISBlox.Services.CLI
 {
    class Program
    {
-      static void Main(string[] args)
+      private static async Task<int> Main(string[] args)
       {
-         Console.WriteLine("Hello World!");
+         var builder = new HostBuilder()
+             .ConfigureServices((hostContext, services) =>
+             {
+               
+             });
+
+         try
+         {
+            return await builder.RunCommandLineApplicationAsync<gbsCmd>(args);
+         }
+         catch (Exception ex)
+         {
+            Console.WriteLine(ex.Message);
+            return 1;
+         }
       }
    }
 }
---
+++ GISBlox.Services.CLI/GISBlox.Services.CLI/gbsCmd.cs
@@ -0,0 +1,33 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GISBlox.Services.CLI
+{
+   [Command(Name = "gbs", OptionsComparison = StringComparison.InvariantCultureIgnoreCase)]
+   [VersionOptionFromMember("--version", MemberName = nameof(GetVersion))]
+   [Subcommand(
+        typeof(LoginCmd),
+        typeof(ListTicketCmd))]
+   class gbsCmd : gbsCmdBase
+   {
+      public gbsCmd(IConsole console)
+      {
+         _console = console;
+      }
+
+      protected override Task<int> OnExecute(CommandLineApplication app)
+      {
+         // this shows help even if the --help option isn't specified
+         app.ShowHelp();
+         return Task.FromResult(0);
+      }
+
+      private static string GetVersion()
+          => typeof(gbsCmd).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+   }
+}
